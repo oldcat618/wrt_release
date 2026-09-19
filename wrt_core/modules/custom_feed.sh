6e627a13aad68a79852e3063ba8c86d50da99b60
oldcat <wwwcat@126.com>
2026-09-19 20:03:56 +0800

    Update custom_feed.sh
    Signed-off-by: oldcat <wwwcat@126.com>

--- wrt_core/modules/custom_feed.sh
+++ wrt_core/modules/custom_feed.sh
@@ -368,6 +368,7 @@ install_custom_feed() {
         "sbwml/luci-app-mosdns|https://github.com/sbwml/luci-app-mosdns.git|v5|mosdns luci-app-mosdns"
         "Openwrt-Passwall/openwrt-passwall|https://github.com/Openwrt-Passwall/openwrt-passwall.git|main|luci-app-passwall"
         "nikkinikki-org/OpenWrt-nikki|https://github.com/nikkinikki-org/OpenWrt-nikki.git|main|nikki luci-app-nikki mihomo-meta"
+        "Openwrt-Fancontrol/openwrt-fancontrol|https://github.com/JiaY-shi/fancontrol.git|main|luci-app-fancontrol"
     )
 
     feeds_path=$(get_feeds_path)
